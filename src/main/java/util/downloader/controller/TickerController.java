7f05f7bcda4355e9dda5012077ce1a3ce04f75ac
ubuntu <ubuntu@ud2>
2022-05-02 12:47:03 +0100

    Organize imports

--- src/main/java/util/downloader/controller/TickerController.java
+++ src/main/java/util/downloader/controller/TickerController.java
@@ -1,5 +1,7 @@
 package util.downloader.controller;
 
+import static util.downloader.util.Constants.API_TOKEN;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -12,13 +14,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.google.gson.Gson;
 
 import util.downloader.dao.PhoenixDAO;
 import util.downloader.model.Ticker;
-import static util.downloader.util.Constants.*;
 
 
 @SuppressWarnings("rawtypes")
